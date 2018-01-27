fa7ca1ff93de0ba07ac11e286eb6649937f7c178
HugoDorbes <35874521+HugoDorbes@users.noreply.github.com>
2018-01-27 18:59:59 +0100

    Update and rename resolution.m to Resolution.m

--- Resolution.m
+++ Resolution.m
@@ -1,21 +1,26 @@
+% construction image avec résoltions différentes
+
 
 close all; clear all; clc;
+
+
 %% Pixel Resolution %%
 Image = imread('mountains.jpg'); 
 size(Image)
 Image = rgb2gray(Image);
 imwrite(Image,'MountainsGris.jpg')
+
 [m,n,~] = size(Image); 
 ni = n/m;
 mpix = [40, 100, 300, 532];
 npix = zeros(length(mpix),1);
+
 for k = 1:length(mpix)
     i = mpix(k);
     p = imresize(Image, [i,i*ni]);
     npix(k) = size(p,2);
     filename = strcat('Pix',num2str(k), '.jpg');
-    imwrite(p,filename)
-    
+    imwrite(p,filename)   
 end
 
 %% Spatial Resolution %%
