8edf5d7e7a972a2cafbf94067999956487b46522
HugoDorbes <35874521+HugoDorbes@users.noreply.github.com>
2018-01-27 18:53:57 +0100

    Update couleur.m

--- couleur.m
+++ couleur.m
@@ -1,30 +1,38 @@
+% Canaux de l'image de montagne
+
 
 close all; clear all; clc;
-%% image %%
-Image = imread('mountains.jpg');
-Image_gray = rgb2gray(Image);
-Image_hsv = rgb2hsv(Image);
-imwrite(Image_gray,'MontGris.jpg');
-imwrite(Image_hsv,'MontHsv.jpg')
 
-R = Image(:,:,1);
-G = Image(:,:,2);
-B = Image(:,:,3);
+%% image %%
+Image = imread('mountains.jpg'); % image originale
+Image_gray = rgb2gray(Image); % image en nuance de gris
+
+imwrite(Image_gray,'MontGris.jpg');
+
+
+R = Image(:,:,1); %compo rouge
+G = Image(:,:,2); %compe verte
+B = Image(:,:,3); %compo bleu
+
 %% Show Parts %%
 C = zeros(size(Image, 1), size(Image, 2));
-CR = cat(3, R, C, C);
-CG = cat(3, C, G, C);
-CB = cat(3, C, C, B);
-CI = cat(3, R, G, B);
+CR = cat(3, R, C, C); % canal rouge
+CG = cat(3, C, G, C); % canal vert
+CB = cat(3, C, C, B); % canal bleu
+CI = cat(3, R, G, B); % image reconstruite
 
 figure
-
 imshow(CR)
 %imwrite(CR,'Red.jpg')
+
 figure
 imshow(CG)
 %imwrite(CG,'Green.jpg')
+
 figure
 imshow(CB)
 %imwrite(CB,'Blue.jpg')
+
+figure
+imshow(CI)
 %imwrite(CI,'Image.jpg')
